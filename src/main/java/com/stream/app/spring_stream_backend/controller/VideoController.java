257cd6b283c46db314b507a17793f2ec35870ade
choudharymanisha <choudharymanisha38566@gmail.com>
2024-10-15 20:08:16 +0530

    Initial commit

--- src/main/java/com/stream/app/spring_stream_backend/controller/VideoController.java
+++ src/main/java/com/stream/app/spring_stream_backend/controller/VideoController.java
@@ -3,6 +3,7 @@ package com.stream.app.spring_stream_backend.controller;
 import com.stream.app.spring_stream_backend.entities.Video;
 import com.stream.app.spring_stream_backend.playload.CustomMessage;
 import com.stream.app.spring_stream_backend.services.VideoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,18 +24,20 @@ public class VideoController {
         this.videoService = videoService;
     }
     @PostMapping
-    public ResponseEntity<CustomMessage> create(@RequestParam("file") MultipartFile file, @RequestParam("title") String title,
+    public ResponseEntity<?> create(@RequestParam("file") MultipartFile file, @RequestParam("title") String title,
                                                 @RequestParam("description") String description) {
 
         Video video = new Video();
         video.setTitle(title);
         video.setDescription(description);
         video.setVideoId(UUID.randomUUID().toString());
-        videoService.save(video,file);
+        Video savedVideo =  videoService.save(video,file);
+        if (savedVideo != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(video);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(CustomMessage.builder().message("Video not uploaded ").success(false).build());
+        }
 
 
-
-videoService.save(video,file);
-return null;
     }
-}
+    }
--- src/main/java/com/stream/app/spring_stream_backend/services/impl/VideoServiceImpl.java
+++ src/main/java/com/stream/app/spring_stream_backend/services/impl/VideoServiceImpl.java
@@ -1,6 +1,7 @@
 package com.stream.app.spring_stream_backend.services.impl;
 
 import com.stream.app.spring_stream_backend.entities.Video;
+import com.stream.app.spring_stream_backend.repositories.VideoRepository;
 import com.stream.app.spring_stream_backend.services.VideoService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,8 +12,10 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 @Service
@@ -20,6 +23,12 @@ public class VideoServiceImpl implements VideoService {
 
 @Value("${files.video}")
 String DIR;
+    private VideoRepository videoRepository;
+    public VideoServiceImpl(VideoRepository videoRepository) {
+        this.videoRepository = videoRepository;
+    }
+
+
 @PostConstruct
 public void init(){
     File file = new File(DIR);
@@ -46,13 +55,18 @@ public void init(){
 
 
            Path path  = Paths.get(cleanFolder,cleanFileName);
+           Files.copy(inputStream,path, StandardCopyOption.REPLACE_EXISTING);
+           video.setContentType(contentType);
+           video.setFilePath(path.toString());
+            return videoRepository.save(video);
 
        } catch(IOException e){
            e.printStackTrace();
+           return null;
 
         }
 
-        return null;
+
     }
 
     @Override
